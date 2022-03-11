3d0f88d94e29df2aceb47cd65487041dafb2e6a5
juliuscaesar169 <juliocsanchez169@gmail.com>
2022-03-11 22:05:54 +0100

    Fix: arrow-back

--- components/AuthMenuComponent.js
+++ components/AuthMenuComponent.js
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import globalStyles from '../styles/styles';
-import authStyles from '../styles/authStyles';
+import React, { Component } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import globalStyles from "../styles/styles";
+import authStyles from "../styles/authStyles";
 
-export class AuthMenuComponent extends Component {
-    
-    render() {
-        return (
-            <View
-                style={[styles.arrowSpace, globalStyles.arrowContainer, globalStyles.row, globalStyles.alignItemsCenter]}
-            >
+const AuthMenuComponent = () => {
+    const navigator = useNavigation();
+  {
+    return (
+      <View
+        style={[
+          styles.arrowSpace,
+          globalStyles.arrowContainer,
+          globalStyles.row,
+          globalStyles.alignItemsCenter,
+        ]}
+      >
+        <TouchableOpacity onPress={() => navigator.goBack()}>
+          <MaterialIcons name="arrow-back-ios" style={globalStyles.arrow} />
+        </TouchableOpacity>
 
-                <TouchableOpacity
-                    onPress={() => useNavigation().goBack()}
-                    style={globalStyles.backArrow}
-                >
-                    <MaterialIcons name="arrow-back-ios" style={globalStyles.arrow} />
-                </TouchableOpacity>
-
-                <Image
-                    style={[authStyles.logoInline, globalStyles.menuLogo]}
-                    source={require('../assets/icon-long.png')} />
-
-            </View>
-        );
-    }
-}
+        <Image
+          style={[authStyles.logoInline, globalStyles.menuLogo]}
+          source={require("../assets/icon-long.png")}
+        />
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
+  arrowSpace: {
+    marginBottom: 28,
+  },
+});
 
-    arrowSpace: {
-        marginBottom: 28
-    }
-
-})
+export default AuthMenuComponent;
