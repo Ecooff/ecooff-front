36ebc954d18145dcab91ca2f7bca32c7de72b76a
unknown <valentinlamothecoulomme@hotmail.com>
2022-03-29 16:33:30 -0300

    put my ip in api

--- services/UrlPath.js
+++ services/UrlPath.js
@@ -1,22 +1,23 @@
-export const LOCALHOST = "http://localhost:3000/api/";
-export const PROD = "http://192.168.100.1:3000/api/";
-export const ANDROID_EMULATOR = "http://10.0.2.2:3000/api/"
+export const LOCALHOST = 'http://localhost:3000/api/';
+export const PROD = 'http://192.168.100.1:3000/api/';
+export const ANDROID_EMULATOR = 'http://10.0.2.2:3000/api/';
+export const IP_LOCALHOST_VALEN = 'http://192.168.100.206:3000/api/';
 
-export const URL = ANDROID_EMULATOR;
+export const URL = IP_LOCALHOST_VALEN;
 
 //Users
-export const serverAlive = URL + "users/serverAlive";
-export const login = URL + "users/authenticate";
-export const signUp = URL + "users/register";
+export const serverAlive = URL + 'users/serverAlive';
+export const login = URL + 'users/authenticate';
+export const signUp = URL + 'users/register';
 
 //Providers
-export const getAllProviders = URL + "providers/";
+export const getAllProviders = URL + 'providers/';
 // Close to expire
-export const closeToExp = URL + "stock/closeToExp";
+export const closeToExp = URL + 'stock/closeToExp';
 // For you (destacados)
-export const forYou = URL + "stock/forYou";
+export const forYou = URL + 'stock/forYou';
 // Get by user Id
-export const getByUserId = URL + "orders/getByUserId";
+export const getByUserId = URL + 'orders/getByUserId';
 
 const URLPath = {
   serverAlive,
