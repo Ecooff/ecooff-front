c7c6ac8b34a43801774f921e4c41fe915a7aa7d1
Tobías Matarasso <42041453+tobiasmatarasso@users.noreply.github.com>
2022-06-16 19:11:31 -0300

    Fix register kewboard

--- screens/Auth/SignupScreen.js
+++ screens/Auth/SignupScreen.js
@@ -77,149 +77,148 @@ const SignupScreen = () => {
     ]);
 
   return (
-    <View>
-      <ScrollView>
-        <View>
-          <AuthMenuComponent />
-          <KeyboardAvoidingView
-            style={[globalStyles.scrollContainer]}
-            keyboardVerticalOffset={headerHeight}
-            // behavior={Platform.OS === "ios" ? "padding" : "height"}
-            // behavior="padding"
-          >
-            {/* MENU */}
 
-            {/* WELCOME MESSAGE */}
-            <View style={authStyles.titleContainer}>
-              <Text style={[authStyles.title, globalStyles.fontLarge]}>
-                Registro
-              </Text>
-              <Text style={[authStyles.subTitle, globalStyles.fontMedium]}>
-                Crea tu cuenta
-              </Text>
-            </View>
+    <ScrollView>
 
-            {/* INPUTS */}
-            <View style={globalStyles.widthEightyFive}>
-              <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
-                NOMBRE Y APELLIDO
-              </Text>
+      <KeyboardAvoidingView
+        style={[globalStyles.scrollContainer]}
+        behavior='padding'
+      >
 
-              <View style={styles.inputColsContainter}>
-                <View style={styles.inputCols}>
-                  <AntDesign name="user" style={globalStyles.icons} />
-                  <TextInput
-                    placeholder="Nombre"
-                    value={firstName}
-                    keyboardType="email-address"
-                    icon="mail"
-                    onChangeText={(text) =>
-                      setFirstName(commonFunctions.capitalize(text))
-                    }
-                    style={globalStyles.input}
-                  />
-                </View>
+        {/* MENU */}
+        <AuthMenuComponent />
 
-                <View style={styles.inputCols}>
-                  <TextInput
-                    placeholder="Apellido"
-                    value={lastName}
-                    keyboardType="email-address"
-                    icon="mail"
-                    onChangeText={(text) =>
-                      setLastName(commonFunctions.capitalize(text))
-                    }
-                    style={globalStyles.input}
-                  />
-                </View>
-              </View>
-
-              <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
-                EMAIL
-              </Text>
-              <View style={globalStyles.inputRow}>
-                <AntDesign name="mail" style={globalStyles.icons} />
-                <TextInput
-                  placeholder="Email"
-                  value={email}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                  icon="mail"
-                  onChangeText={(text) => setEmail(text.toLowerCase())}
-                  style={globalStyles.input}
-                />
-              </View>
-
-              <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
-                CONTRASEÑA
-              </Text>
-              <View style={globalStyles.inputRow}>
-                <AntDesign name="lock" style={globalStyles.icons} />
-                <TextInput
-                  placeholder="Contraseña"
-                  value={password}
-                  onChangeText={(text) => setPassword(text)}
-                  style={globalStyles.input}
-                  secureTextEntry
-                />
-              </View>
-
-              <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
-                REPETIR CONTRASEÑA
-              </Text>
-              <View style={globalStyles.inputRow}>
-                <AntDesign name="lock" style={globalStyles.icons} />
-                <TextInput
-                  placeholder="Repetí tu contraseña"
-                  value={repetePassword}
-                  onChangeText={(text) => setRepetePassword(text)}
-                  style={globalStyles.input}
-                  secureTextEntry
-                />
-              </View>
-            </View>
-
-            {/* BUTTONS */}
-            <View
-              style={[authStyles.buttonContainer, globalStyles.widthEightyFive]}
-            >
-              <TouchableOpacity
-                onPress={() => signUp()}
-                // disabled={firstName != null && lastName != null && email != null && password != null && repetePassword != null && loader != null}
-                style={[
-                  globalStyles.button,
-                  globalStyles.primary,
-                  globalStyles.widthFluid,
-                ]}
-              >
-                {loader == false < 2 ? (
-                  <View
-                    style={[globalStyles.container, globalStyles.horizontal]}
-                  >
-                    <ActivityIndicator size="small" color="#FFF" />
-                  </View>
-                ) : (
-                  <Text style={[styles.loginText, globalStyles.textWhite]}>
-                    Resgistrarme
-                  </Text>
-                )}
-              </TouchableOpacity>
-            </View>
-
-            {/* FINAL LABEL */}
-            <TouchableOpacity
-              onPress={() => navigator.navigate("Login")}
-              style={[styles.finalLabel, authStyles.registerContainter]}
-            >
-              <Text style={[authStyles.headerTitle, globalStyles.fontSmall]}>
-                ¿Ya tenés cuenta?
-                <Text style={globalStyles.fontBold}> Inicia sesión</Text>
-              </Text>
-            </TouchableOpacity>
-          </KeyboardAvoidingView>
+        {/* WELCOME MESSAGE */}
+        <View style={authStyles.titleContainer}>
+          <Text style={[authStyles.title, globalStyles.fontLarge]}>
+            Registro
+          </Text>
+          <Text style={[authStyles.subTitle, globalStyles.fontMedium]}>
+            Crea tu cuenta
+          </Text>
         </View>
-      </ScrollView>
-    </View>
+
+        {/* INPUTS */}
+        <View style={globalStyles.widthEightyFive}>
+          <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
+            NOMBRE Y APELLIDO
+          </Text>
+
+          <View style={styles.inputColsContainter}>
+            <View style={styles.inputCols}>
+              <AntDesign name="user" style={globalStyles.icons} />
+              <TextInput
+                placeholder="Nombre"
+                value={firstName}
+                keyboardType="email-address"
+                icon="mail"
+                onChangeText={(text) =>
+                  setFirstName(commonFunctions.capitalize(text))
+                }
+                style={globalStyles.input}
+              />
+            </View>
+
+            <View style={styles.inputCols}>
+              <TextInput
+                placeholder="Apellido"
+                value={lastName}
+                keyboardType="email-address"
+                icon="mail"
+                onChangeText={(text) =>
+                  setLastName(commonFunctions.capitalize(text))
+                }
+                style={globalStyles.input}
+              />
+            </View>
+          </View>
+
+          <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
+            EMAIL
+          </Text>
+          <View style={globalStyles.inputRow}>
+            <AntDesign name="mail" style={globalStyles.icons} />
+            <TextInput
+              placeholder="Email"
+              value={email}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              icon="mail"
+              onChangeText={(text) => setEmail(text.toLowerCase())}
+              style={globalStyles.input}
+            />
+          </View>
+
+          <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
+            CONTRASEÑA
+          </Text>
+          <View style={globalStyles.inputRow}>
+            <AntDesign name="lock" style={globalStyles.icons} />
+            <TextInput
+              placeholder="Contraseña"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              style={globalStyles.input}
+              secureTextEntry
+            />
+          </View>
+
+          <Text style={[globalStyles.inputLabel, globalStyles.fontSmall]}>
+            REPETIR CONTRASEÑA
+          </Text>
+          <View style={globalStyles.inputRow}>
+            <AntDesign name="lock" style={globalStyles.icons} />
+            <TextInput
+              placeholder="Repetí tu contraseña"
+              value={repetePassword}
+              onChangeText={(text) => setRepetePassword(text)}
+              style={globalStyles.input}
+              secureTextEntry
+            />
+          </View>
+        </View>
+
+        {/* BUTTONS */}
+        <View
+          style={[authStyles.buttonContainer, globalStyles.widthEightyFive]}
+        >
+          <TouchableOpacity
+            onPress={() => signUp()}
+            // disabled={firstName != null && lastName != null && email != null && password != null && repetePassword != null && loader != null}
+            style={[
+              globalStyles.button,
+              globalStyles.primary,
+              globalStyles.widthFluid,
+            ]}
+          >
+            {loader == false < 2 ? (
+              <View
+                style={[globalStyles.container, globalStyles.horizontal]}
+              >
+                <ActivityIndicator size="small" color="#FFF" />
+              </View>
+            ) : (
+              <Text style={[styles.loginText, globalStyles.textWhite]}>
+                Resgistrarme
+              </Text>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        {/* FINAL LABEL */}
+        <TouchableOpacity
+          onPress={() => navigator.navigate("Login")}
+          style={[styles.finalLabel, authStyles.registerContainter]}
+        >
+          <Text style={[authStyles.headerTitle, globalStyles.fontSmall]}>
+            ¿Ya tenés cuenta?
+            <Text style={globalStyles.fontBold}> Inicia sesión</Text>
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+    </ScrollView>
+
   );
 };
 
